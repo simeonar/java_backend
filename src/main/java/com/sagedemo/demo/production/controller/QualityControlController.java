361c23a13aeb219dfe037109363f6db568d448dc
Semen Arshyn <semenarshyn@gmail.com>
2025-07-10 12:44:52 +0200

    Add quality control: QualityControl entity, repository, service, controller (basic CRUD)

---
+++ src/main/java/com/sagedemo/demo/production/controller/QualityControlController.java
@@ -0,0 +1,23 @@
+package com.sagedemo.demo.production.controller;
+
+import com.sagedemo.demo.production.entity.QualityControl;
+import com.sagedemo.demo.production.service.QualityControlService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/production/quality-control")
+public class QualityControlController {
+    private final QualityControlService qualityControlService;
+    public QualityControlController(QualityControlService qualityControlService) {
+        this.qualityControlService = qualityControlService;
+    }
+    @PostMapping
+    public QualityControl createQC(@RequestBody QualityControl qc) {
+        return qualityControlService.createQualityControl(qc);
+    }
+    @GetMapping
+    public List<QualityControl> getAllQC() {
+        return qualityControlService.getAllQualityControls();
+    }
+}
